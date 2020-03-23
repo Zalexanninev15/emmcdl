c38630e463bdd81dea2f5a1564085bec3eeadecc
Zalexanninev15 <maks.shabarchin@yandex.ru>
2020-03-23 08:44:28 +0300

    Improvements

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# build it
-# Autoconf 2.69
-# Automake 1.15
-
 aclocal
 autoconf
 automake --add-missing
-
 ./configure
-
-make # emmcdl linux binary in .
+make
+sudo emmcdl /usr/bin/emmcdl
+echo "OK!"
